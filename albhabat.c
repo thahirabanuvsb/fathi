b448eaef5553e3080ebe549da146a3f98a77f561
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-01-21 07:20:01 -0300

    Update and rename leap.c to albhabat.c

---
+++ albhabat.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+  scanf("%d",&a);
+ if("a>='a')&&("a<='z')||("a>='A')&&("a<='Z')
+ {
+  printf("its albha");
+ }
+ else
+ {
+  printf("its not albha");
+ }
+ return0;
+}
+ 
+    
+  
