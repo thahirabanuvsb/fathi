266a0ebfb5693a23fe0d8d3e67f2d324c27f291f
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-01-23 10:56:24 +0000

    Update and rename prime.c to oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+  printf("enter the odd no 1to 50");
+  
+  for(i=1;i<=50;i++)
+    if(i%2==0)
+    {
+      printf("no is even");
+    }
+  else
+  {
+    printf("ni is odd");
+  }
+  return0;
+}
