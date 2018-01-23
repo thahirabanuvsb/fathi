ab89196d7f2f20db228a4878f582c5808ca43f52
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-01-23 11:15:07 +0000

    Update and rename evenno.c to prti.c

---
+++ prti.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,j,c;
+  printf("enter the no prime bt two intervals")
+    for("i=1;i<=10;i++")
+      for("j=1;j<=100;j++")
+        if(i%j==0)
+        {
+          c++;
+          count==0;
+          printf("no is prime");
+   
+        }
+  else
+  {
+  printf("no is not prime");
+  }
+  return0;
+}
