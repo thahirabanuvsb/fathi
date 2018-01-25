f67632390665cda86c354ba4f6b1e67753307af1
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-01-25 11:25:41 +0000

    Update and rename amstrongno.c to amstbt.c

---
+++ amstbt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{ 
+  int n,i,temp,m,num remain;
+  printf("enter the no bt two intervals");
+  scanf("%d","%d",&m,&n);
+  for("i=1;i<=n;i++")
+  {
+    temp=i;
+    num=0;
+    while(temp!=0)
+    {
+      rem=temp/10;
+      num=rem*rem*rem;
+      temp/10
+    }
+    if(n==0)
+    {
+      printf("no is amstrong");
+      return0;
+    }
+  }
+  
