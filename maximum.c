10625040375af220cba61c692097f7f203ac0859
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-01 14:25:15 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+inta[5],n,i,max;
+printf("enter the no ");
+scanf("%d",&n);
+max=a[0];
+for(i=0;i<=n;i++)
+if(a[i]>max)
+{
+max=a[i];
+}
+printf("no is max");
+return 0;
+}
+
