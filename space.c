523a1a643a56a394b4c2ec1a95bb3b97476933a5
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-02 15:02:48 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+char[7]="hello";
+int i,count=0;
+for(i=0;a[i]!='/0';i++)
+{
+if(a[i]="")
+{
+count++;
+}
+}
+printf("the no of spaces are %d",count);
+return 0;
+}
