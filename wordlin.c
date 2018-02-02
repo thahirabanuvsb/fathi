5877eb75f650faaabe9e8593d1e34f9602599496
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-02 14:50:38 +0530

    Create wordlin.c

---
+++ wordlin.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+
+
+{
+char s[200],i;
+printf("enter string");
+scanf("%c",'s');
+
+for(i=0;s(i)!=' ';i++)
+{
+	
+	printf("coubnt the string%d",i)
+}
+
+return 0;
+}
+
+
+    
+    
+
+	
+
+
