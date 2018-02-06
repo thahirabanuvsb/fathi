05044ab1129d4f10f43abb4477677a3db1efe919
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-06 15:06:38 +0530

    Rename maxten.c to among.c

---
+++ among.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,max,size;
+scanf("%d",&size);
+printf("enter the no");
+for(i=0;i<=size;i++)
+scanf("%d",&ar[i]);
+max=ar[i];
+if(max<ar[i])
+{
+max=ar[i];
+}
+printf("%d",max);
+return 0;
+}
