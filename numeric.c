b4cad4639f31dba8f554d353cd4d8979e52dc8bd
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-08 14:48:39 +0530

    Update and rename spacein.c to numeric.c

---
+++ numeric.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int i,count=0;
+	gets(p);
+	char p[45];
+	for("i=0;p[i]!='\0';i++);
+	    {
+		    if((p[i]>='0'&&(p[i]<='8'))
+		       {
+			       count++;
+		       }
+		       printf("%d",count);
+		       }
+	
+	return 0;
+	
+
+}
