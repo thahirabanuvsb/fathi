0b52dee8c226bc539084fded4817b5aba1289150
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-02-08 14:17:30 +0530

    Update and rename swapping.c to swapbit.c

---
+++ swapbit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	printf("\n enter the value i");
+	scanf("%d",&i);
+	printf("\n enter the value j");
+	scanf("%d",&j);
+	{
+		i=i^j;
+		j=i^j;
+		i=i^j;
+		printf("after the swap value");
+		printf("before swap value"):
+	}
+	
+	return 0;
+}
