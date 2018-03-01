fe639bc8d48e0ac060d199ea92389f340d630b16
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-03-01 15:51:18 +0530

    Create givenonme.c

---
+++ givenonme.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+switch(a)
+{
+case1:
+      printf("one")
+      break;
+case2:
+      printf("two");
+      break;
+case3:
+      printf("three");
+      break;
+default:
+      printf("invalid no");
+      break;
+return 0;
+}
