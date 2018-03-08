a35dd484fa367360ce893cb3d9380428868bf58a
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-03-08 16:09:54 +0530

    Create even near.c

---
+++ even near.c
@@ -0,0 +1,18 @@
+#include<stdio.h.
+int main()
+[
+int a;
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d",a);
+printf("even");
+}
+else
+{
+printf("%d",a-1);
+printf("nearest eveen no");
+}
+return 0;
+}
+
