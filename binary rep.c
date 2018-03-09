13cce52b8c702b366cba54c5d236db8cffdf7dd7
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-03-09 11:00:26 +0000

    Update and rename greatstno.c to binary rep.c

---
+++ binary rep.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+int i,count=0,lenth=0;
+chara[50];
+scanf("%d",&a[i]);
+for(i=0;s[i]!='\0';i++)
+{
+length++;
+if(s[i]=='0'||s[i]=='1')
+count++;
+}
+if(count==length)
+{
+	printf("yes");
+}
+else	
+{
+printf("no");	
+}
+return 0;
+}
+
+
