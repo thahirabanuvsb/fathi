ba4ae92133f0b8438cf75edb0053a293b7aabc7b
thahirabanuvsb <31505175+thahirabanuvsb@users.noreply.github.com>
2018-03-09 11:12:28 +0000

    Update and rename binary rep.c to eveodd.c

---
+++ eveodd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("\n%d",c);
+	if(c%2==0)
+	{
+		printf("\n even");
+	}
+else
+{
+	printf("odd");
+}
+
+return 0;
+}
+
+
+
+
+
